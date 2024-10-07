6d339b8142aadda9e9d65470dd2797b61881d453
vtonu <victor.tonu@yahoo.com>
2024-10-07 09:04:25 -0700

    Back to Top button

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -23,7 +23,7 @@ const Contacts = ({ homeRef }) => {
           Email - meterelectrical@gmail.com
           <button
             onClick={() => copyToClipboard('meterelectrical@gmail.com')}
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-200 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-100 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
             <span className="relative transition-all duration-75 ease-in bg-white rounded-md group-hover:bg-opacity-0">
               <Copy className="flex w-6 h-6 px-1 py-1 shadow-sm rounded-xs text-neutral-900 shadow-blue-300" />
             </span>
@@ -33,7 +33,7 @@ const Contacts = ({ homeRef }) => {
           Website - www.meterelectrical.com
           <button
             onClick={() => copyToClipboard('www.meterelectrical.com')}
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-200 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-100 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
             <span className="relative transition-all duration-75 ease-in bg-white rounded-md group-hover:bg-opacity-0">
               <Copy className="flex w-6 h-6 px-1 py-1 shadow-sm rounded-xs text-neutral-900 shadow-blue-300" />
             </span>
@@ -43,7 +43,7 @@ const Contacts = ({ homeRef }) => {
           Phone - (425)-588-7578
           <button
             onClick={() => copyToClipboard('4255887578')}
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-200 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-100 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
             <span className="relative transition-all duration-75 ease-in bg-white rounded-md group-hover:bg-opacity-0">
               <Copy className="flex w-6 h-6 px-1 py-1 shadow-sm rounded-xs text-neutral-900 shadow-blue-300" />
             </span>
@@ -53,7 +53,7 @@ const Contacts = ({ homeRef }) => {
       <div className="button-container">
         <button
           onClick={() => homeRef.current?.scrollIntoView({ behavior: 'smooth' })}
-          className="flex px-2 py-2 mx-2 rounded-sm shadow-sm text-neutral-900 shadow-blue-300 bg-gradient-to-br from-blue-600 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-200 hover:text-black dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
+          className="flex px-2 py-2 mx-2 rounded-sm shadow-sm text-neutral-900 shadow-blue-300 bg-gradient-to-br from-blue-500 to-blue-100 group-hover:from-sky-500 group-hover:to-blue-100 hover:text-blue-100 dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
           <MoveUp />
           Back to Top
         </button>
