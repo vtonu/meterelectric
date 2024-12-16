1d1bf3cfb3ad98b9d74e22ad1c85ef87803d4985
vtonu <56773210+vtonu@users.noreply.github.com>
2024-12-15 17:23:05 -0800

    Updated back to top button color.

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -53,7 +53,7 @@ const Contacts = ({ homeRef }) => {
       <div className="button-container">
         <button
           onClick={() => homeRef.current?.scrollIntoView({ behavior: 'smooth' })}
-          className="flex px-2 py-2 mx-2 rounded-sm shadow-sm text-neutral-800 shadow-blue-300 bg-gradient-to-br from-blue-600 to-blue-50 group-hover:from-sky-300 group-hover:to-blue-200 hover:text-blue-100 dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
+          className="flex px-2 py-2 mx-2 rounded-sm shadow-sm text-neutral-800 shadow-blue-500 bg-gradient-to-br from-blue-600 to-blue-500 group-hover:from-sky-300 group-hover:to-blue-200 hover:text-blue-100 dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-sky-800">
           <MoveUp />
           Back to Top
         </button>
