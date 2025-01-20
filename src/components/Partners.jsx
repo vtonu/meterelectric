c867c7ced42118eb2dc989d9f8417ae15419f2e7
vtonu <56773210+vtonu@users.noreply.github.com>
2025-01-19 21:54:39 -0800

    Certified Generac Installer

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,10 +1,10 @@
-import { partners } from '../constants';
+import { partners } from "../constants";
 
 const Partners = () => {
   return (
     <div className="mt-20 tracking-wide min-h-[300px] border-t border-blue-700">
       <h2 className="pt-20 mt-6 text-5xl tracking-wide text-center sm:text-5xl lg:text-6xl">
-        OUR{' '}
+        OUR{" "}
         <span className="text-transparent bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text">
           PARTNERS
         </span>
@@ -14,13 +14,23 @@ const Partners = () => {
         {partners.map((partner, index) => (
           <div key={index} className="w-full px-4 py-2 sm:w-1/2 lg:w-1/3">
             <div className="p-4 border-2 border-blue-700 rounded-md bg-neutral-800">
-              <a href="https://www.generac.com" target="_blank" rel="noopener noreferrer">
-                <img className="w-full rounded-md h-30 object-fit" src={partner.image} />
+              <a
+                href="https://www.generac.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="w-full rounded-md h-30 object-fit"
+                  src={partner.image}
+                />
               </a>
             </div>
           </div>
         ))}
       </div>
+      <span className="text-transparent bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text text-center">
+        Certified Generac Installer
+      </span>
     </div>
   );
 };
